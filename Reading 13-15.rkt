7ce34949be838465851ef973125ee0a7fb383d23
socrateszhang <sophiezhanguiuc@gmail.com>
2016-10-03 13:11:17 -0400

    chap13-15

---
+++ Reading 13-15.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Reading 13-15|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "convert.rkt" "teachpack" "htdp") (lib "guess.rkt" "teachpack" "htdp") (lib "guess-gui.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "convert.rkt" "teachpack" "htdp") (lib "guess.rkt" "teachpack" "htdp") (lib "guess-gui.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;; Reading 13-15
+;;; Section 13
+;; List Abreviations (list a b c)
+;;; Section 14
+;; 14.1 Structures in Structures
+
+;; 14.2 Binary Search Trees
+
+;; 14.3 Lists in Lists
